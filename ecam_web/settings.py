f62b24b854b46da1016fe1ae5fd70b410be7b33c
MichaelHBaker <mbaker@sbwconsulting.com>
2025-03-29 08:24:43 -0700

    static root

--- ecam_web/settings.py
+++ ecam_web/settings.py
@@ -148,6 +148,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+# This defines where collectstatic will gather files for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
